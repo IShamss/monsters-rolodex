2ad7d5555aaea30da7bd1f82991e8558f47a7756
IShamss <mohamedshams2000@gmail.com>
2020-03-20 17:36:07 +0200

    Updates

---
+++ precache-manifest.d6210bb039b0d294523790c3bd5d6941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3afdaeb67c3fb2b200147d7a6ba5c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b1db90a67b71dc6a00fd",
+    "url": "/monsters-rolodex/static/css/main.2f86f618.chunk.css"
+  },
+  {
+    "revision": "f11931c9bca0585edf2b",
+    "url": "/monsters-rolodex/static/js/2.38b7acc7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.38b7acc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1db90a67b71dc6a00fd",
+    "url": "/monsters-rolodex/static/js/main.a57c6d99.chunk.js"
+  },
+  {
+    "revision": "7e8033ca9e87f4cbf3bf",
+    "url": "/monsters-rolodex/static/js/runtime-main.f655cd06.js"
+  }
+]);
